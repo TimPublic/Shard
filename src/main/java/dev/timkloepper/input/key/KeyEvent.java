bef8cbf41c55b7763220995081ec25e1f33216f6
Revan1017 <timkloepper.public@gmail.com>
2026-03-31 18:39:40 +0200

    Added Mouse and Key listener with respective events.

---
+++ src/main/java/dev/timkloepper/input/key/KeyEvent.java
@@ -0,0 +1,23 @@
+package dev.timkloepper.input.key;
+
+import dev.timkloepper.event_system.I_Event;
+
+public class KeyEvent implements I_Event {
+
+    public KeyEvent(KEY_EVENT_TYPE type, Character key, int scanCode) {
+        TYPE = type;
+        KEY = key;
+        SCAN_CODE = scanCode;
+    }
+
+    public enum KEY_EVENT_TYPE {
+        PRESS,
+        RELEASE,
+        REPEAT,
+    }
+
+    public final KEY_EVENT_TYPE TYPE;
+    public final Character KEY;
+    public final int SCAN_CODE;
+
+}
